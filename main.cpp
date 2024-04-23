93fbd71c87d2905bf21f5714d002e8522b8a0ea0
TheSemiColin <146498846+TheSemiColin@users.noreply.github.com>
2024-04-23 16:00:46 -0500

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
 #include "SDL_Plotter.h"
 #include "Grid.h"
 #include "Square.h"
@@ -10,8 +11,9 @@ int main(int argc, char ** argv) {
     TYPE type;
     SDL_Plotter g(SIZE,SIZE);
     grid _grid(SIZE, SIZE, SIDE, color(0,0,0));
-    square sq1;
+    bool started = false;
     square data[DIM][DIM];
+    int time = clock();
 
     //Init Data
     for (int r = 0; r < DIM; r++) {
@@ -23,30 +25,45 @@ int main(int argc, char ** argv) {
     while (!g.getQuit()) {
 		if(g.kbhit()){
             switch(toupper(g.getKey())) {
-                case 'E': type = EMPTY;
-		                  break;
-                case 'C': type = CACTUS;
-                          break;
-                case 'O': type = OASIS;
-                          break;
-                case 'R': type = RABBIT;
-                          break;
-                case 'S': type = SNAKE;
-                          break;
-                case 'H': type = HAWK;
-                          break;
-                case 'D': allToDefault(data);
-                          type = EMPTY;
-                          break;
+                case 'E':
+                    type = EMPTY;
+                    break;
+                case 'C':
+                    type = CACTUS;
+                    break;
+                case 'O':
+                    type = OASIS;
+                    break;
+                case 'R':
+                    type = RABBIT;
+                    break;
+                case 'S':
+                    type = SNAKE;
+                    break;
+                case 'H':
+                    type = HAWK;
+                    break;
+                case 'D':
+                    if (!started) {
+                        allToDefault(data);
+                        type = EMPTY;
+                    }
+                    break;
+                case 'P':
+                    started = true;
+                    break;
+                default:
+                    type = EMPTY;
+                    break;
 		    }
 		}
 
-		if(g.mouseClick()){
+		if(g.mouseClick() && !started){
 		    point p = (g.getMouseClick());
 		    data[p.y/SIDE][p.x/SIDE].setType(type);
 		}
 
-		for (int r = 0; r < DIM; r++) {
+          for (int r = 0; r < DIM; r++) {
             for (int c = 0; c < DIM; c++) {
                 data[r][c].draw(g);
             }
