513690bdfbcc2e69f2d38c1a17888cd6fcbe4b61
Jay Chou <153050535+Jayceon573971@users.noreply.github.com>
2024-05-02 10:47:25 -0500

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -26,7 +26,8 @@ public:
     /*
      * brief Constructor for the grid class.
      *
-     * This constructor initializes a grid object with the specified number of rows, columns,
+     * This constructor initializes a grid object with the 
+     specified number of rows, columns,
      * size of each cell, and color of the grid lines.
      *
      * param r Number of rows in the grid.
