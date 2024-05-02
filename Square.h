d4d513af93c63d86fb2a6b545e6b4d550ec06140
Jlock27 <166733628+Jlock27@users.noreply.github.com>
2024-05-02 10:50:22 -0500

    Add files via upload
    Added most of the header descriptions

--- Square.h
+++ Square.h
@@ -22,23 +22,139 @@ private:
 public:
     square();
 
+    /*
+     * description: Sets the type of each individual square
+     * return: void
+     * precondition: ENUM TYPE declared
+     * postcondition: sets the type of a square
+     *
+    */
     void setType(TYPE);
+    /*
+     * description: Sets the color of each individual square
+     * return: void
+     * precondition: color declared
+     * postcondition: sets the color of a square
+     *
+    */
     void setColor(color);
+    /*
+     * description: Sets the row each square appears on
+     * return: void
+     * precondition:
+     * postcondition: sets the row of a square
+     *
+    */
     void setRow(int);
+    /*
+     * description: Sets the collumn each square appears on
+     * return: void
+     * precondition:
+     * postcondition: sets the column of a square
+     *
+    */
     void setCol(int);
+    /*
+     * description: Sets the health of each character
+     * return: void
+     * precondition:
+     * postcondition: sets the health of a square
+     *
+    */
     void setHealth(int);
+    /*
+     * description: Adds one hp to a square
+     * return: void
+     * precondition:
+     * postcondition: Adds one to health
+     *
+    */
     void updateHealth(int);
 
+    /*
+     * description: Returns the type of a square
+     * return: TYPE
+     * precondition: Type is set
+     * postcondition: Gets the type of a square
+     *
+    */
     TYPE getType() const;
+    /*
+     * description: Gets the color each square appears on
+     * return: color
+     * precondition: Color is set
+     * postcondition: gets the health of a square
+     *
+    */
     color getColor() const;
+    /*
+     * description: Gets the row each square appears on
+     * return: int
+     * precondition: Row is set
+     * postcondition: Gets the row of a square
+     *
+    */
     int getRow() const;
+    /*
+     * description: Gets the column each square appears on
+     * return: int
+     * precondition: Column is set
+     * postcondition: Gets the column of a square
+     *
+    */
     int getCol() const;
+    /*
+     * description: Gets the health each square appears on
+     * return: int
+     * precondition: Health is set
+     * postcondition: Gets the health of a square
+     *
+    */
     int getHealth() const;
 
+    /*
+     * description: Draws the color of each square in the grid
+     * return: void
+     * precondition:
+     * postcondition: Draws the colors for each square
+     *
+    */
     void draw(SDL_Plotter& g, SEASON&);
+    /*
+     * description: Selects a square and scans squares in the rows and columns
+     * return: char
+     * precondition: SEASON enum set
+     * postcondition: Returns an char based on the type of squares in row and
+     *                columns
+     *
+    */
     char scan(square[][DIM], int, int);
+    /*
+     * description: Moves the square in the x direction if scan returns a
+     *              cactus or oasis
+     * return: int
+     * precondition: scan was run
+     * postcondition: Moves a square in x direction
+     *
+    */
     int moveINX(square[][DIM], int, int);
+    /*
+     * description: Moves the square in the y direction if scan returns a
+     *              cactus or oasis
+     * return: int
+     * precondition: scan was run
+     * postcondition: Moves a square in y direction
+     *
+    */
     int moveINY(square[][DIM], int, int);
+    /*
+     * description: Swaps the current square type with the empty type it
+     *              moves to
+     * return: int
+     * precondition: scan was run
+     * postcondition: Swaps the position of two squares
+     *
+    */
     void move(int, int);
     void kill(square[][DIM], int, int);
     void reproduce(square[][DIM], int, int);
